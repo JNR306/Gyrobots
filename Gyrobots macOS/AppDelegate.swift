7c5df6098de2b70047a50f1d1b932b73071d3d65
Jan-Niklas Röhlig <138626680+JNR306@users.noreply.github.com>
2025-12-18 20:26:02 +0100

    Initial Commit

---
+++ Gyrobots macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  Gyrobots macOS
+//
+//  Created by Jan-Niklas Röhlig on 18.12.25.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
